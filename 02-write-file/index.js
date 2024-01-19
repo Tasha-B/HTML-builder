cddb550b0b96f78ff3a2b8153e8144c64060376a
Natalia Belyatskaya <natbeliatsky@gmail.com>
2024-01-19 16:39:58 +0300

    feat: add 02-write-file solution

---
+++ 02-write-file/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+const { stdin, stdout, exit } = process;
+let writableStream = fs.createWriteStream(path.join(__dirname, 'file.txt'));
+
+stdout.write('Hello! Please, enter your message:\n');
+stdin.on('data', (data) => {
+  if (data.toString().trim() === 'exit') {
+    stdout.write('OK! Welcome!');
+    exit();
+  } else {
+    writableStream.write(data);
+  }
+});
+
+process.on('SIGINT', () => {
+  stdout.write('OK! Welcome!');
+  exit();
+});
