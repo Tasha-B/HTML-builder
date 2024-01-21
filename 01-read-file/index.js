f997282394e247b3b14ab6d6b1c27468a1586ab7
Natalia Belyatskaya <natbeliatsky@gmail.com>
2024-01-22 00:29:47 +0300

    feat: add 05-merge-styles solution

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const path = require('path');
 path.join(__dirname, '01-read-file');
 
-const ReadableStream = new fs.createReadStream(
+const readableStream = new fs.createReadStream(
   '01-read-file/text.txt',
   'utf-8',
 );
-ReadableStream.on('data', (chunk) => console.log(chunk));
+readableStream.on('data', (chunk) => console.log(chunk));
--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,1 +1,30 @@
 const fs = require('fs');
+const path = require('path');
+const writableStream = fs.createWriteStream(
+  path.join(__dirname, 'project-dist', 'bundle.css'),
+);
+
+fs.readdir(
+  path.join(__dirname, 'styles'),
+  { withFileTypes: true },
+  (error, data) => {
+    if (error) throw error;
+
+    data.forEach((file) => {
+      fs.stat(
+        path.join(__dirname, 'styles', file.name),
+        function (error, stat) {
+          if (error) throw error;
+
+          if (stat.isFile() && path.extname(file.name) === '.css') {
+            const readableStream = fs.createReadStream(
+              path.join(__dirname, 'styles', file.name),
+              'utf-8',
+            );
+            readableStream.pipe(writableStream);
+          }
+        },
+      );
+    });
+  },
+);
